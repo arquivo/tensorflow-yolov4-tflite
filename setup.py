3d0aeb593fcf12e5dc501425be57b2226bca40fb
André Mourão <andre.mourao@fccn.pt>
2020-08-06 13:58:18 +0100

    More data

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='tfyolo',
 	version='1.0',
 	description='tfyolo',
-	packages=['tfyolo','tfyolo.core'],
+	packages=['tfyolo','tfyolo.core','tfyolo.data'],
 	include_package_data=True,
 	package_data={
         "data": ["*.txt", "*.names"],
